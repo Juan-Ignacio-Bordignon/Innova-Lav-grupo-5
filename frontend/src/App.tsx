a802201b9310e88133505f307b847468d7abbf39
Emma Akai <EmmaAkaiGit@gmail.com>
2026-05-18 17:28:12 -0300

    feat: inicializar proyecto frontend con React, TypeScript, Vite y Tailwind

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <main className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <section className="rounded-2xl bg-white p-8 shadow-md text-center">
+        <h1 className="text-3xl font-bold text-gray-900">
+          Frontend - InnovaLab Grupo 5
+        </h1>
+
+        <p className="mt-4 text-gray-600">
+          React + TypeScript + Vite + Tailwind funcionando.
+        </p>
+      </section>
+    </main>
+  )
+}
+
+export default App
