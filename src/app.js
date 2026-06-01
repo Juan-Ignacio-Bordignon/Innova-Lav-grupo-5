2e125b012bd5f858af1bb5962193bb369f0f751f
Juan-Ignacio-Bordignon <juanibordil@gmail.com>
2026-06-01 13:10:19 -0300

    Enrutado de "module" - mock

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 import express from "express";
 import cors from "cors";
 
+import authRoutes from "./routes/auth.routes.js";
+import moduleRoutes from "./routes/module.routes.js";
+
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Routes
+app.use("/auth", authRoutes);
+app.use("/module", moduleRoutes);
 
 export default app;
