45401b6e5ecd3b6266a4589ea9ea4ada8a0fae33
Juan-Ignacio-Bordignon <juanibordil@gmail.com>
2026-06-12 18:44:49 -0300

    feat: agregar validaciones y middleware para el registro e inicio de sesión de usuarios

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { postRegister, postLogin } from "../controllers/auth.controller.js";
+import {
+  registerValidation,
+  loginValidation,
+} from "../validators/auth.validator.js";
+import { validateFields } from "../middlewares/validate.middleware.js";
 
 const router = Router();
 
-router.post("/register", postRegister);
-router.post("/login", postLogin);
+router.post("/register", registerValidation, validateFields, postRegister);
+router.post("/login", loginValidation, validateFields, postLogin);
 
 export default router;
