e072cdba232707c07430db049a404f050dc688d0
Juan-Ignacio-Bordignon <juanibordil@gmail.com>
2026-06-24 20:17:19 -0300

    fix: Actualizar la documentación de la API y refactorizar el controlador de favoritos para utilizar un token en la identificación del usuario.

--- src/controllers/favorite.controller.js
+++ src/controllers/favorite.controller.js
@@ -1,32 +1,41 @@
 import { PrismaClient } from "@prisma/client";
+import { verifyToken } from "../utils/jws.js";
+
 const prisma = new PrismaClient();
 
 // POST /favorites → agregar favorito
 export const addFavorite = async (req, res) => {
   try {
-    const { userId, leccionId } = req.body;
+    const token = req.headers.authorization.split(" ")[1];
+    const userId = verifyToken(token);
+
+    const { leccionId } = req.body;
     const favorite = await prisma.favorite.create({
       data: {
         userId: parseInt(userId),
-        leccionId: parseInt(leccionId)
-      }
+        leccionId: parseInt(leccionId),
+      },
     });
     res.json({ mensaje: "Lección agregada a favoritos", favorite });
   } catch (e) {
     if (e.code === "P2002") {
-      return res.status(400).json({ error: "Esta lección ya está en favoritos" });
+      return res
+        .status(400)
+        .json({ error: "Esta lección ya está en favoritos" });
     }
     res.status(500).json({ error: "No se pudo agregar a favoritos" });
   }
 };
 
-// GET /favorites/:userId → traer favoritos del usuario
+// GET /favorites → traer favoritos del usuario
 export const getFavorites = async (req, res) => {
   try {
-    const { userId } = req.params;
+    const token = req.headers.authorization.split(" ")[1];
+    const userId = verifyToken(token);
+
     const favoritos = await prisma.favorite.findMany({
       where: { userId: parseInt(userId) },
-      include: { leccion: true }
+      include: { leccion: true },
     });
     res.json({ favorites: favoritos });
   } catch (e) {
@@ -39,7 +48,7 @@ export const deleteFavorite = async (req, res) => {
   try {
     const { id } = req.params;
     await prisma.favorite.delete({
-      where: { id: parseInt(id) }
+      where: { id: parseInt(id) },
     });
     res.json({ mensaje: "Lección eliminada de favoritos" });
   } catch (e) {
