7ad9b37fc9795736a5ff3c3b24d0b8998e5d660a
Juan-Ignacio-Bordignon <juanibordil@gmail.com>
2026-06-30 17:09:25 -0300

    feat: Implementar el controlador de lecciones y actualizar las rutas de los módulos.

---
+++ src/controllers/lesson.controller.js
@@ -0,0 +1,42 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+// GET /module/:moduleId/lessons
+export const getLessons = async (req, res) => {
+  try {
+    const { moduleId } = req.params;
+    const lecciones = await prisma.leccion.findMany({
+      where: { moduloId: parseInt(moduleId) },
+    });
+    res.json({ lessons: lecciones });
+  } catch (e) {
+    res
+      .status(500)
+      .json({
+        error:
+          "No se pudieron obtener las lecciones para el módulo especificado",
+      });
+  }
+};
+
+// GET /module/:moduleId/lessons/:lessonId
+export const getLesson = async (req, res) => {
+  try {
+    const { moduleId, lessonId } = req.params;
+    const leccion = await prisma.leccion.findFirst({
+      where: {
+        id: parseInt(lessonId),
+        moduloId: parseInt(moduleId),
+      },
+    });
+    if (!leccion)
+      return res
+        .status(404)
+        .json({ error: "No se pudo obtener la lección especificada" });
+    res.json({ lesson: leccion });
+  } catch (e) {
+    res
+      .status(500)
+      .json({ error: "No se pudo obtener la lección especificada" });
+  }
+};
--- src/routes/module.routes.js
+++ src/routes/module.routes.js
@@ -1,14 +1,13 @@
 import { Router } from "express";
-import {
-  getModules,
-  getLessons,
-  getLesson,
-} from "../controllers/module.controller.js";
+import { getModules } from "../controllers/module.controller.js";
+import { getLessons, getLesson } from "../controllers/lesson.controller.js";
+import { getExercises } from "../controllers/exercise.controller.js";
 
 const router = Router();
 
 router.get("/", getModules);
 router.get("/:moduleId/lessons", getLessons);
 router.get("/:moduleId/lessons/:lessonId", getLesson);
+router.get("/:moduleId/lessons/:lessonId/exercises", getExercises);
 
 export default router;
