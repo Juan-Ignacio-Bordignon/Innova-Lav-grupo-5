15c17aac79f4d52859efb36e20793ee875231252
Juan-Ignacio-Bordignon <juanibordil@gmail.com>
2026-07-03 13:08:49 -0300

    fix: Eliminar el registro de la consola del manejo de errores de getModules

--- src/controllers/module.controller.js
+++ src/controllers/module.controller.js
@@ -9,7 +9,6 @@ export const getModules = async (req, res) => {
     });
     res.json({ modules: modulos });
   } catch (e) {
-    console.log(e);
     res.status(500).json({ error: "No se pudieron obtener los módulos" });
   }
 };
