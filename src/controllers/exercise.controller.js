10d97c7b20b5d0f1195887dfe8f8584ae65e1117
Juan-Ignacio-Bordignon <juanibordil@gmail.com>
2026-07-06 12:13:47 -0300

    fix: Corrección por conflicto de merge de ramas

--- src/controllers/exercise.controller.js
+++ src/controllers/exercise.controller.js
@@ -2,7 +2,8 @@ import { PrismaClient } from "@prisma/client";
 import { recuperarEstadoDeEjercicio } from "../services/exercise.service.js";
 
 const prisma = new PrismaClient();
-const URL_BASE_VIDEOS = "https://ozrcernencngontkultp.supabase.co/storage/v1/object/public/videos-lsa/";
+const URL_BASE_VIDEOS =
+  "https://ozrcernencngontkultp.supabase.co/storage/v1/object/public/videos-lsa/";
 // URL Base para Supabase Storage (Bucket público: videos-lsa)
 
 // GET /module/:moduleId/lessons/:lessonId/exercises
@@ -10,26 +11,31 @@ export const getExercises = async (req, res) => {
   try {
     const { moduleId, lessonId } = req.params;
     const LessonExercises = await getExercisesBylessonId(lessonId);
-    
-    // ⚠️ Control original del proyecto (Evita mapear si no hay ejercicios)
+
+    //  Validación  para lecciones sin ejercicios cargados
     if (!LessonExercises || LessonExercises.length === 0) {
-      return res.status(404).json({
-        error: "No se pudo obtener los ejercicios de la lección especificada",
+      return res.status(200).json({
+        exercises: [],
+        _meta: {
+          status: "inDevelopment",
+          message:
+            "Esta lección se encuentra en desarrollo. Próximamente se añadirán ejercicios multimedia de LSA.",
+        },
       });
     }
 
     // Inyección de URL de Supabase: Convertimos los strings planos en URLs reales (.mp4)
-    const exercisesConUrlReal = LessonExercises.map(exercise => {
+    const exercisesConUrlReal = LessonExercises.map((exercise) => {
       return {
         ...exercise,
-        contenidoMultimedia: `${URL_BASE_VIDEOS}${exercise.contenidoMultimedia}.mp4`
+        contenidoMultimedia: `${URL_BASE_VIDEOS}${exercise.contenidoMultimedia}.mp4`,
       };
     });
 
     const authorization = req.headers.authorization;
     let exercises = await recuperarEstadoDeEjercicio(
       authorization,
-      exercisesConUrlReal
+      exercisesConUrlReal,
     );
 
     res.status(200).json({ exercises });
